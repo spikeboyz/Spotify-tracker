d5bfb974de17384c7852d024c0bfa1832c6072dc
Jacob Rodriguez <jacobrodriguez@Jacobs-Air.attlocal.net>
2023-03-06 15:09:15 -0500

    commit 4

--- index.py
+++ index.py
@@ -19,6 +19,11 @@ def main():
     #merges all the files into a list
     master_list = merge_files(json_files)
 
+    #groups the songs by month in a dictionary
+    monthly_stats = group_by_month(master_list)
+
+    rankings = rank(monthly_stats)
+
     print(master_list)
     
 
@@ -36,7 +41,53 @@ def merge_files(json_files):
 
     return result
 
+def group_by_month(master_list):
+    """
+    Takes in the master list of the combined json files and returns a list
+    of dictionaries where the key is the date in year/month and the value
+    is a list of the songs listlened to in that month
+    """
 
+    monthly_stats = dict()
+
+    for song in master_list:
+        timestamp = song['ts']
+
+        #I use timestamp[:7] because spotify json files use "YYYY-MM" so
+        #you have to get the first 7 digits including the dash
+        year_month = timestamp[:7]
+
+        #extract the name of the song
+        name = song['master_metadata_track_name']
+
+        #makes a new key value pair for every month
+        if year_month not in monthly_stats:
+            monthly_stats[year_month] = []
+        monthly_stats[year_month].append(name)
+
+    return monthly_stats
+
+def rank(monthly_stats):
+    """
+    Takes in a dictionary of the the songs divided by month and returns 
+    a dictionary with the keys being the year and month and the values 
+    being another dictionary with the songs and the amount of times it's gotten it played
+    """
+    song_rankings = dict()
+
+    #makes it a touple with the first object being the year-month and the scond the list of songs
+    for month, songs in monthly_stats.items():
+            ranking = dict()
+            for song in songs:
+                name = song['master_metadata_track_name']
+                if name in ranking:
+                    ranking[name] += 1
+                else:
+                    ranking[name] = 1
+            sorted_ranking = dict(sorted(ranking.items(), key=lambda item: item[1], reverse=True))
+            song_rankings[month] = sorted_ranking
+
+    return song_rankings
 
 if __name__ == "__main__":
     main()
