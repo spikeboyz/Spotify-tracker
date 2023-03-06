72dc0726bdedf5009e53f22f59df30c611938014
Jacob Rodriguez <jacobrodriguez@Jacobs-Air.attlocal.net>
2023-03-05 23:49:05 -0500

    third commit

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import datetime
 import json
 
 
@@ -7,17 +7,34 @@ def main():
     #ask for the folder
     folder = input("input folder with spotify json files:")
 
-    files = os.listdir(folder)
+    # creates the full path to the folder
+    full_path = os.path.expanduser(folder)
 
-    json_files = [f for f in files if f.endswith('.json')]
+    # finds all files in the folder
+    files = os.listdir(full_path)
 
-    merge_files(json_files)
+    # puts all the json files from the folder into a list
+    json_files = [os.path.join(full_path, f) for f in files if f.endswith('.json')]
+
+    #merges all the files into a list
+    master_list = merge_files(json_files)
+
+    print(master_list)
+    
+
+
 
 def merge_files(json_files):
+    """
+    Merges json files from a list of json files to a single
+    list containing the merged json file
+    """
     result = list()
     for file in json_files:
          with open(file, 'r') as infile:
             result.extend(json.load(infile))
+
+    return result
 
 
 
